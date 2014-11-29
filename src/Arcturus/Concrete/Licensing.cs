0135afa7d889c9ed11ee65e6122e63d1bad78e6d
Christopher Shepherd <chris.shepherd@outlook.com>
2014-11-29 18:44:33 +0000

    Exposed a nice static method to return the current license

--- src/Arcturus/Concrete/Licensing.cs
+++ src/Arcturus/Concrete/Licensing.cs
@@ -4,7 +4,7 @@ namespace Arcturus.Concrete
 {
     public class Licensing
     {
-        internal static ILicenseProvider Instance { get; set; }
+        public static ILicenseProvider Instance { get; set; }
 
         public static void SetLicenseProvider(ILicenseProvider provider)
         {
